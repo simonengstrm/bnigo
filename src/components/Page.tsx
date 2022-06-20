5f4d7d5dd5b3de4124f738075f7974c5fa2e5812
Simon Engström <simon_0_0_@live.se>
2022-06-20 20:44:16 +0200

    Bingo fast felstavat

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -17,7 +17,7 @@ function Head() {
     <div className="place-items-center text-center w-full m-auto py-4">
       <a href="/">
         <span className="text-5xl font-bold">Bnigo</span><br/>
-        <span className="font-thin">When you need to bingo your heart out ❤️</span>
+        <span className="font-thin">Bingo fast felstavat 💕</span>
       </a>
     </div>
   )
