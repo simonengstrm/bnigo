8b7e08374a7694b0b3464ffa1ec092e69948db97
LilleLink <simon_0_0_@live.se>
2022-08-11 22:36:04 +0200

    Makes login page more functional

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, ChangeEventHandler, useState } from "react";
+import React, { ChangeEvent, ChangeEventHandler, useState, useEffect, FormEvent } from "react";
 import Page from "../../src/components/Page";
 import { useRouter } from "next/router";
 
@@ -12,12 +12,18 @@ export default function Login() {
   const [error, setError] = useState("");
   const [signupMode, setSignupMode] = useState(false);
 
+  useEffect(() => {
+    document.querySelectorAll("input").forEach((input) => {
+      input.value = "";
+    });
+  }, [signupMode])
+
   function isValidForm() {
     return !(form.username == "" || form.password == "");
   }
 
-  async function submitForm() {
-
+  async function submitForm(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
     const apiURL : string = signupMode ? "/api/user/signup" : "/api/auth/login";
 
     const result = await fetch(apiURL, {
@@ -30,11 +36,13 @@ export default function Login() {
       })
     });
 
-    if(result.status == 200) {
+    if(result.status == 200 && !signupMode) {
       router.push(new URL("/user", document.baseURI))
     } else {
       setError((await result.json()).error);
     }
+
+    if (signupMode) setSignupMode(!signupMode);
   }
 
   function handleChange(event : ChangeEvent<HTMLInputElement>) {
@@ -50,28 +58,31 @@ export default function Login() {
 
   return (
     <Page>
-      <div className="flex w-full justify-center">
-        <div className="flex w-1/2 bg-white p-4 rounded-xl shadow-lg ">
-          <div className="flex space-y-4 flex-col w-full place-items-center">
-            <span className="text-3xl font-bold">{signupMode ? "Sign up" : "Log in"}</span>
-            <div className="flex flex-col w-full">
-              <span className="text-sm">Username</span>
-              <input type="text" name="username" value={form.username} onChange={handleChange} className="h-8 rounded-md grow border"/>
+      <form onSubmit={e => submitForm(e)}>
+        <div className="flex w-full justify-center">
+          <div className="flex w-1/2 bg-white p-4 rounded-xl shadow-lg ">
+            <div className="flex space-y-4 flex-col w-full place-items-center">
+              <span className="text-3xl font-bold">{signupMode ? "Sign up" : "Log in"}</span>
+              
+              <div className="flex flex-col w-full">
+                <span className="text-sm">Username</span>
+                <input type="text" name="username" value={form.username} onChange={handleChange} className="h-8 rounded-md grow border"/>
+              </div>
+              <div className="flex flex-col w-full">
+                <span className="text-sm">Password</span>
+                <input type="password" name="password" value={form.password} onChange={handleChange} className="h-8 rounded-md grow border"/>
+              </div>
+              {isValidForm() ? 
+                <button type="submit" className="cursor-pointer w-full p-2 rounded-full text-center bg-green-600 hover:bg-green-500 text-white">{getCurrentModeName()}</button>
+                :
+                <button type="submit" disabled className="cursor-pointer w-full p-2 rounded-full text-center bg-gray-400 text-black">{getCurrentModeName()}</button>
+              }
+              {error == "" ? <></> : <span className="text-red-500 text-sm">{error}</span>}
+              <div className="text-center">Not a member? <a href="#" className="underline text-blue-600" onClick={() => setSignupMode(!signupMode)}>Click here</a>!</div>
             </div>
-            <div className="flex flex-col w-full">
-              <span className="text-sm">Password</span>
-              <input type="password" name="password" value={form.password} onChange={handleChange} className="h-8 rounded-md grow border"/>
-            </div>
-            {isValidForm() ? 
-              <button onClick={submitForm} className="cursor-pointer w-full p-2 rounded-full text-center bg-green-600 hover:bg-green-500 text-white">{getCurrentModeName()}</button>
-              :
-              <button disabled onClick={submitForm} className="cursor-pointer w-full p-2 rounded-full text-center bg-gray-400 text-black">{getCurrentModeName()}</button>
-            }
-            {error == "" ? <></> : <span className="text-red-500 text-sm">{error}</span>}
-            <div className="text-center">Not a member? <a href="#" onClick={() => setSignupMode(!signupMode)}>Click here</a>!</div>
           </div>
         </div>
-      </div>
+      </form>
     </Page>
   )
 
