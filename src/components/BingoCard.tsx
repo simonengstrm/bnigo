4bc2ca0d96587701e8804a933514801050edeb0d
LilleLink <simon_0_0_@live.se>
2022-09-19 13:05:11 +0200

    Custom scaling

--- src/components/BingoCard.tsx
+++ src/components/BingoCard.tsx
@@ -3,7 +3,7 @@ import { Bingo } from "../../lib/types"
 export default function BingoCard(props : { bingo : Bingo }) {
   return (
     <a href={"/game/"+props.bingo.name}>
-      <div className="w-full text-center place-items-center p-4 shadow-2xl hover:scale-105 transition ease-linear bg-white">
+      <div className="w-full text-center place-items-center p-4 shadow-2xl hover:scale-101 transition ease-linear bg-white">
         <h1 className="text-lg font-semibold">{props.bingo.name}</h1>
         <p>{props.bingo.description}</p>
         <p>Antal frågor: {props.bingo.questions.length}</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      'scale': {
+        '101': '1.01',
+      }
+    },
   },
   plugins: [],
 }
